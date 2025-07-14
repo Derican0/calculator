1088b32e71bda236021e06ab18b7b0584fc5dda8
Derican0 <ericzhang@student.unimelb.edu.au>
2025-07-14 23:30:46 +1000

    Add math operators

---
+++ script.js
@@ -0,0 +1,41 @@
+// Math operators
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        // TODO: Handle error
+        return;
+    }
+    return a / b;
+}
+
+// Variables
+let a;
+let b;
+let operator;
+
+function operate(operator, a, b) {
+    if (operator == "+") {
+        return add(a, b);
+    }
+    else if (operator == "-") {
+        return subtract(a, b);
+    }
+    else if (operator == "*") {
+        return multiply(a, b);
+    }
+    else if (operator == "/") {
+        return divide(a, b);
+    }
+}
